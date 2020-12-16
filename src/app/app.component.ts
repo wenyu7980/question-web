d9650afd400b9bf6a585b8e5d2cd6a7bfc7c8c17
wenyu <wenyu7980@163.com>
2020-12-16 22:31:57 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FormControl} from '@angular/forms';
+import {debounceTime, filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'question';
+  questions: Question[];
+  private location: Storage;
+  question: Question;
+
+  index = 0;
+  answer: string;
+  gotoIndex: FormControl;
+
+  constructor(private httpClient: HttpClient) {
+    this.location = localStorage;
+  }
+
+  ngOnInit(): void {
+    this.index = +this.location.getItem('index');
+    this.httpClient.get<Question[]>('/assets/questions.json').subscribe(
+      data => {
+        this.questions = data;
+        if (this.index >= this.questions.length) {
+          this.index = this.questions.length - 1;
+        }
+        this.goto(this.index);
+      }
+    );
+    this.gotoIndex = new FormControl();
+    this.gotoIndex.valueChanges.pipe(
+      debounceTime(100),
+      filter(i => i !== '')
+    ).subscribe(i => {
+      this.goto(i - 1);
+    });
+  }
+
+  pre() {
+    this.goto(this.index - 1);
+  }
+
+  next() {
+    this.goto(this.index + 1);
+  }
+
+  goto(index: number) {
+    this.index = index;
+    this.question = this.questions[this.index];
+    this.answer = '';
+    this.location.setItem('index', index + '');
+  }
 }
+
+interface Question {
+  question: string;
+  answer: string;
+  analysis: string;
+  options: string[];
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NZ_I18N, zh_CN} from 'ng-zorro-antd/i18n';
+import {registerLocaleData} from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import {NzButtonModule, NzDividerModule, NzInputModule, NzInputNumberModule, NzRadioModule} from 'ng-zorro-antd';
+import {NzSpaceModule} from 'ng-zorro-antd/space';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzRadioModule,
+    NzButtonModule,
+    NzSpaceModule,
+    NzInputNumberModule,
+    NzInputModule,
+    NzDividerModule
   ],
-  providers: [],
+  providers: [{provide: NZ_I18N, useValue: zh_CN}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
